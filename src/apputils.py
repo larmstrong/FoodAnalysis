507d9977967a535afa53d6f65e4479610e785bbe
Leonard Armstrong <lta100163@gmail.com>
2019-07-13 14:08:19 -0400

    .gitignore: Added Log file. apputils.py: Added load_/get_/set_configuration routines. Added create_/get_logger routines. config.yaml: Added section for logging configuration information. main.py: Updated global variable names to lowercase. Renamed FoodGraph to load_food_graph and added comments. Added some logging. Added some comments. Added food graph loading.

--- src/apputils.py
+++ src/apputils.py
@@ -2,9 +2,100 @@
 Provides a set of generic utilites that should be applicable to this of a variety of other projects.
 """
 
+# Define the intended exported entities.
+__all__ = [ 'read_configuration', 'set_configuration', 'get_configuration', 'create_logger' ]
+
 #--------------------------------------------------------------------------------------------------
 # Module and library imports
 
+import io
+import logging
 import os
+import yaml
 
-app_directory = os.get_
+#--------------------------------------------------------------------------------------------------
+# Define module constants
+
+_app_directory = os.getcwd()
+#src_directory = os.path.join(app_directory, 'src')
+
+_configuration = {}
+
+#--------------------------------------------------------------------------------------------------
+# Define exported configuration functions
+
+def read_configuration (path = None) :
+    """
+    read_configuration: Reads configuration data from a YAML configuration file.
+    Parameters:
+      path: Path to the configuration file. If no parameter is passed then the configuration file
+        is assumed to be named `config.yaml` and to reside in the current working directory.
+    Returns: The configuration file that was read.
+    """
+    global _configuration
+
+    # Use path if one is provided. Otherwise assume config.yaml in current working directory.
+    cfg_path = path if path is not None else os.path.join(_app_directory, 'config.yaml')
+    cfg_stream = io.open(cfg_path, 'r')
+
+    # Load and return the configuration.
+    _configuration = yaml.safe_load(cfg_stream)
+    return _configuration
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def set_configuration (cfg) :
+    global _configuration
+    _configuration = cfg
+    return _configuration
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def get_configuration () :
+    global _configuration
+    return _configuration
+
+#--------------------------------------------------------------------------------------------------
+# Define exported configuration functions
+
+def create_logger () :
+    """
+    create_logger: Creates an application-specific logger with output to the console and to a file.
+    """
+    # Get the application logger name from the configuration.
+    food_config = get_configuration()
+    logger_config = food_config['logging']
+    logname = logger_config['log-name']
+
+    # Create a logger object
+    logger = logging.getLogger(logname)
+    logger.setLevel(logging.INFO)
+
+    # Define the logging handlers.
+    # Only do the handler work if none already exists.
+    #if len(logger.handlers) == 0 :
+    # Get the default console handler.
+    ch = logging.StreamHandler()
+
+    # Create a file handler as well
+    logpath = os.path.join(_app_directory, logname + '.txt')
+    fh = logging.FileHandler(logpath)
+    fh.setLevel(logging.INFO)
+
+    # Create a formatter and add it to the handlers
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s : %(message)s')
+    ch.setFormatter(formatter)
+    fh.setFormatter(formatter)
+
+    # Add the handlers to logger
+    logger.addHandler(ch)
+    logger.addHandler(fh)
+
+    return logger
+
+def get_logger () :
+    cfg = get_configuration()
+    logcfg = cfg['logging']
+    loggername = logcfg['log-name']
+    logger = logging.getLogger(name=loggername)
+    return logger
--- src/main.py
+++ src/main.py
@@ -6,52 +6,78 @@
 
 import io
 import os
+import sys
 import yaml
 
 from neobolt.exceptions import ServiceUnavailable
-from py2neo import Graph
+import py2neo
 
+# The following constants must be created before we can import local modules since they live in a
+# ./src subdirectory.
+app_directory = os.getcwd()
+src_directory = os.path.join(app_directory, 'src')
 
+# Append the src directory to the Python path.
+sys.path.append(src_directory)
 
-import sys
+import apputils
 
 #--------------------------------------------------------------------------------------------------
 # Define global constants
 
-CONFIG_FILEPATH = os.path.join(os.getcwd(), 'src', 'config.yaml')
-CONFIG_STREAM = io.open(CONFIG_FILEPATH, 'r')
-FOOD_CONFIG = yaml.safe_load(CONFIG_STREAM)
-food_gr = Graph()
+# app_directory: Defined earlier
+# src_directory: Defined earlier
+config_filepath = os.path.join(src_directory, 'config.yaml')
+
+#food_config = yaml.safe_load(config_stream)
+food_config = apputils.read_configuration(config_filepath)
+food_gr = py2neo.Graph()
 
 #--------------------------------------------------------------------------------------------------
 # FoodGraph
 
-def FoodGraph (authentication, host, port, protocol):
-    return(Graph(auth=authentication, host=host, port=port, scheme=protocol))
+def load_food_graph (authentication, host, port, protocol):
+    """
+    load_food_graph: Loads the food graph database from Neo4J.
+    Parameters:
+      authentication: Two-par credentials tuple (username and password) used to authenticate to
+        the Neo4J food database.
+      host: Name of the host server where the Neo4J database is active.
+      port: Communication port of the Neo4J database on the host server.
+      protocol: Application-layer protocol used to communicate with the Neo4J database.
+    """
+    return(py2neo.Graph(auth=authentication, host=host, port=port, scheme=protocol))
 
 #--------------------------------------------------------------------------------------------------
 # Main module
 
 def main():
+    # Assure we are using the global food_gr database
     global food_gr
-    global food_db
 
-    neo4j_config = FOOD_CONFIG['neo4j-database']
+    # Read configuration and export required Neo4J values.
+    neo4j_config = food_config['neo4j-database']
     current_version = neo4j_config['dbversion']
-    auth_credentials = (
-        neo4j_config['user'],
-        neo4j_config['pw'])
+    auth_credentials = ( neo4j_config['user'], neo4j_config['pw'] )
     host = neo4j_config['host']
     port = neo4j_config['port']
     protocol = neo4j_config['scheme']
 
+    logger = apputils.create_logger()
+    logger.info('Logging enabled.')
+
     # Connect to the food graph
-    food_gr = FoodGraph(authentication=auth_credentials, host=host, port=port, protocol=protocol)
+    food_gr = load_food_graph(
+        authentication=auth_credentials, host=host, port=port, protocol=protocol)
     # Verify if the DB is active
     try:
         dbname = food_gr.database.name
     except ServiceUnavailable:
         print('Neo4J service not available. Is it running?')
+        # 3 (ESRCH) is the value found in errno.h for "No such process."
+        sys.exit(3)
+
+    # The food graph DB is active. Let's start to process Epicurious
 
 
 if __name__ == '__main__':
